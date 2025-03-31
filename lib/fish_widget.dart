fb52eabefd40fca8afb9ce6c416978a66f4cd74f
meoz23 <laboumyz@gmail.com>
2025-03-31 14:22:14 -0400

    Created fish animation

---
+++ lib/fish_widget.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Fish extends StatefulWidget {
+  final Color color;
+  final double speed;
+
+  Fish({required this.color, required this.speed});
+
+  @override
+  _FishState createState() => _FishState();
+}
+
+class _FishState extends State<Fish> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: (3 ~/ widget.speed)),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    final random = Random();
+    _animation = Tween<Offset>(
+      begin: Offset(random.nextDouble(), random.nextDouble()),
+      end: Offset(random.nextDouble(), random.nextDouble()),
+    ).animate(_controller);
+
+    _controller.addListener(() {
+      if (mounted) setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: _animation.value.dx * 250,
+      top: _animation.value.dy * 250,
+      child: Icon(Icons.fish, color: widget.color, size: 30),
+    );
+  }
+}
